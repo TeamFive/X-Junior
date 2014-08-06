675cf38a803c9b9a9d0f50b9769293426b2d931f
Palina Ukadzer <polina_ukader@mail.ru>
2014-08-06 19:06:53 +0300

    pointer

--- X-Junior-web/src/main/webapp/resources/js/Core/Views/FieldRow.js
+++ X-Junior-web/src/main/webapp/resources/js/Core/Views/FieldRow.js
@@ -1,7 +1,7 @@
 define(["Views/Base", "jquery"],function(Base, $){
     return Base.extend({},{
         defaults: $.extend(true, {}, Base.defaults,{
-            tpl:'<tr><td>{%- data.field.get(\'name\') %}</td><td>{%- data.field.get(\'fieldType\') %}</td><td>{%- data.field.get(\'defaultValue\') %}</td><td>{%- data.field.get(\'fieldLook\') %}</td><td>{%- data.field.get(\'change\') %}</td><td><a><i data-id={%- data.field.get(\'id\') %} class="fa fa-times" alt=(\'Delete\')> </i></a></td></tr>'
+            tpl:'<tr><td>{%- data.field.get(\'name\') %}</td><td>{%- data.field.get(\'fieldType\') %}</td><td>{%- data.field.get(\'defaultValue\') %}</td><td>{%- data.field.get(\'fieldLook\') %}</td><td>{%- data.field.get(\'change\') %}</td><td><a><i data-id={%- data.field.get(\'id\') %} class="fa fa-times curs" alt=(\'Delete\')> </i></a></td></tr>'
        })
     });
 
